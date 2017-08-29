cb73facc18d88b55c7204d276462eee9abb71397
Ethan N. Paul <enpaul@omni.enpaul.net>
2017-08-29 16:30:47 -0400

    Testing more script stuff

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,7 @@
 New-Item -Path "C:\inetpub\wwwroot\" -Name enpaul.net.new -ItemType Directory -Force
-Copy-Item -Path .\* -Destination "C:\inetpub\wwwroot\enpaul.net.new\" -Force -Recurse
-Rename-Item -Path "C:\inetpub\wwwroot\enpaul.net" -NewName "enpaul.net.old" -Force
-Rename-Item -Path "C:\inetpub\wwwroot\enpaul.net.new" -NewName "enpaul.net" -Force
-Remove-Item -path "C:\inetpub\wwwroot\enpaul.net.old" -Recurse -Force
+#Copy-Item -Path .\* -Destination "C:\inetpub\wwwroot\enpaul.net.new\" -Force -Recurse
+Copy-Item -Path .\deploy.ps1 -Destination "C:\inetpub\wwwroot\enpaul.net.new\" -Force -Recurse
+#Rename-Item -Path "C:\inetpub\wwwroot\enpaul.net" -NewName "enpaul.net.old" -Force
+#Rename-Item -Path "C:\inetpub\wwwroot\enpaul.net.new" -NewName "enpaul.net" -Force
+#Remove-Item -path "C:\inetpub\wwwroot\enpaul.net.old" -Recurse -Force
+exit
