7d73b5c045eb5cd1d707e9bad84e7179461d3030
Ethan N. Paul <enpaul@omni.enpaul.net>
2017-08-29 15:51:53 -0400

    Gave up and added ps script for fucker

---
+++ deploy.ps1
@@ -0,0 +1,5 @@
+New-Item -Path "C:\inetpub\wwwroot\" -Name enpaul.net.new -ItemType Directory -Force
+Copy-Item -Path .\* -Destination "C:\inetpub\wwwroot\enpaul.net.new\" -Force -Recurse
+Rename-Item -Path "C:\inetpub\wwwroot\enpaul.net" -NewName "enpaul.net.old" -Force
+Rename-Item -Path "C:\inetpub\wwwroot\enpaul.net.new" -NewName "enpaul.net" -Force
+Remove-Item -path "C:\inetpub\wwwroot\enpaul.net.old" -Recurse -Force
