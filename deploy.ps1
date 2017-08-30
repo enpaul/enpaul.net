8d733b760810584cb6ca4a3ed89c2fe9dae1217f
Ethan N. Paul <enpaul@omni.enpaul.net>
2017-08-29 22:17:48 -0400

    Finalized script for deploy procedure

--- deploy.ps1
+++ deploy.ps1
@@ -4,12 +4,15 @@ if (Test-Path "C:\inetpub\wwwroot\enpaul.net.new") {
 
 New-Item -Path "C:\inetpub\wwwroot\" -Name "enpaul.net.new" -ItemType Directory -Force
 
-#Copy-Item -Path .\* -Destination "C:\inetpub\wwwroot\enpaul.net.new\" -Force -Recurse
-#Remove-Item -Path "C:\inetpub\wwwroot\enpaul.net.new\.git" -Recurse -Force
-#Remove-Item -Path "C:\inetpub\wwwroot\enpaul.net.new\.gitlab-ci.yml" -Force
-#Copy-Item -Path .\deploy.ps1 -Destination "C:\inetpub\wwwroot\enpaul.net.new\" -Force -Recurse
+Copy-Item -Path ".\*" -Destination "C:\inetpub\wwwroot\enpaul.net.new\" -Force -Recurse
+
+Remove-Item -Path "C:\inetpub\wwwroot\enpaul.net.new\.git" -Recurse -Force
+Remove-Item -Path "C:\inetpub\wwwroot\enpaul.net.new\.gitlab-ci.yml" -Force
+Remove-Item -Path "C:\inetpub\wwwroot\enpaul.net.new\deploy.ps1" -Force
+Remove-Item -Path "C:\inetpub\wwwroot\enpaul.net.new\"
+
 Rename-Item -Path "C:\inetpub\wwwroot\enpaul.net" -NewName "enpaul.net.old" -Force
 Rename-Item -Path "C:\inetpub\wwwroot\enpaul.net.new" -NewName "enpaul.net" -Force
+
 Remove-Item -path "C:\inetpub\wwwroot\enpaul.net.old" -Recurse -Force
-#Remove-Item -Path "C:\inetpub\wwwroot\enpaul.net\deploy.ps1" -Force
 exit
