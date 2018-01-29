a813c42d33b3634359a03e8084e9282ae31c17dd
Ethan N. Paul <enpaul@omni.enpaul.net>
2018-01-29 09:33:52 -0500

    Updated ci config to support both iis and apache release and dev

---
+++ .ci-release.ps1
@@ -0,0 +1,29 @@
+if (Test-Path "C:\inetpub\wwwroot\tmp-build.enpaul") {
+  Remove-Item -Path "C:\inetpub\wwwroot\tmp-build.enpaul" -Recurse -Force
+}
+
+New-Item -Path "C:\inetpub\wwwroot\" -Name "tmp-build.enpaul" -ItemType Directory -Force
+
+Copy-Item -Path ".\*" -Destination "C:\inetpub\wwwroot\tmp-build.enpaul\" -Force -Recurse
+
+if (Test-Path "C:\inetpub\wwwroot\tmp-build.enpaul\.git") {
+  Remove-Item -Path "C:\inetpub\wwwroot\tmp-build.enpaul\.git" -Recurse -Force
+}
+if (Test-Path "C:\inetpub\wwwroot\tmp-build.enpaul\.gitlab-ci.yml") {
+  Remove-Item -Path "C:\inetpub\wwwroot\tmp-build.enpaul\.gitlab-ci.yml" -Force
+}
+if (Test-Path "C:\inetpub\wwwroot\tmp-build.enpaul\deploy.ps1") {
+  Remove-Item -Path "C:\inetpub\wwwroot\tmp-build.enpaul\deploy.ps1" -Force
+}
+if (Test-Path "C:\inetpub\wwwroot\tmp-build.enpaul\.gitignore") {
+  Remove-Item -Path "C:\inetpub\wwwroot\tmp-build.enpaul\.gitignore" -Force
+}
+if (Test-Path "C:\inetpub\wwwroot\tmp-build.enpaul\.htaccess") {
+  Remove-Item -Path "C:\inetpub\wwwroot\tmp-build.enpaul\.htaccess" -Force
+}
+
+Rename-Item -Path "C:\inetpub\wwwroot\enpaul" -NewName "enpaul.old" -Force
+Rename-Item -Path "C:\inetpub\wwwroot\tmp-build.enpaul" -NewName "enpaul" -Force
+
+Remove-Item -path "C:\inetpub\wwwroot\enpaul.old" -Recurse -Force
+exit
