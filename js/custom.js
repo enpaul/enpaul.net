7121239bee2958716a6be2b1761743f718c128e0
Ethan N. Paul <enpaul@omni.enpaul.net>
2018-02-03 03:27:26 -0500

    Updated page styling and layout, done save for content and final tweaks

--- js/custom.js
+++ js/custom.js
@@ -1,26 +1,28 @@
 jQuery(document).ready(function( $ ) {
 
+  //alert("You're the 1000th visitor!!!!");
+
   // Preloader
   $(window).on('load', function() {
     $('#preloader').delay(100).fadeOut('slow',function(){$(this).remove();});
   });
 
   // Hero rotating texts
-  $("#hero .rotating").Morphext({
+  $("#top .rotating").Morphext({
     animation: "flipInX",
     separator: ",",
     speed: 3000
   });
-  
+
   // Initiate the wowjs
   new WOW().init();
-  
+
   // Initiate superfish on nav menu
   $('.nav-menu').superfish({
     animation: {opacity:'show'},
     speed: 400
   });
-  
+
   // Mobile Navigation
   if( $('#nav-menu-container').length ) {
       var $mobile_nav = $('#nav-menu-container').clone().prop({ id: 'mobile-nav'});
@@ -29,19 +31,19 @@ jQuery(document).ready(function( $ ) {
       $('body').prepend( '<button type="button" id="mobile-nav-toggle"><i class="fa fa-bars"></i></button>' );
       $('body').append( '<div id="mobile-body-overly"></div>' );
       $('#mobile-nav').find('.menu-has-children').prepend('<i class="fa fa-chevron-down"></i>');
-      
+
       $(document).on('click', '.menu-has-children i', function(e){
           $(this).next().toggleClass('menu-item-active');
           $(this).nextAll('ul').eq(0).slideToggle();
           $(this).toggleClass("fa-chevron-up fa-chevron-down");
       });
-      
+
       $(document).on('click', '#mobile-nav-toggle', function(e){
           $('body').toggleClass('mobile-nav-active');
           $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
           $('#mobile-body-overly').toggle();
       });
-      
+
       $(document).click(function (e) {
           var container = $("#mobile-nav, #mobile-nav-toggle");
           if (!container.is(e.target) && container.has(e.target).length === 0) {
@@ -55,7 +57,7 @@ jQuery(document).ready(function( $ ) {
   } else if ( $("#mobile-nav, #mobile-nav-toggle").length ) {
       $("#mobile-nav, #mobile-nav-toggle").hide();
   }
-  
+
   // Stick the header at top on scroll
   $("#header").sticky({topSpacing:0, zIndex: '50'});
 
@@ -64,13 +66,13 @@ jQuery(document).ready(function( $ ) {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
           var target = $(this.hash);
           if (target.length) {
-              
+
               var top_space = 0;
-              
+
               if( $('#header').length ) {
                 top_space = $('#header').outerHeight();
               }
-              
+
               $('html, body').animate({
                   scrollTop: target.offset().top - top_space
               }, 1500, 'easeInOutExpo');
@@ -85,12 +87,12 @@ jQuery(document).ready(function( $ ) {
                   $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
                   $('#mobile-body-overly').fadeOut();
               }
-              
+
               return false;
           }
       }
   });
-  
+
   // Back to top button
   $(window).scroll(function() {
 
@@ -99,9 +101,9 @@ jQuery(document).ready(function( $ ) {
       } else {
           $('.back-to-top').fadeOut('slow');
       }
-      
+
   });
-  
+
   $('.back-to-top').click(function(){
       $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
       return false;
